136261471ca8a649e2a627d5890ad9f8420d4a04
chandravishaal <chandravishal9911.com>
2025-08-07 20:58:18 +0530

    feat: project setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">🚀 Tailwind + React + Vite works!</h1>
+    </div>
+  )
+}
